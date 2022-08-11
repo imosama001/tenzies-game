f54948839821a28ebcf26d4e5261a858a0c01b41
mohd osama raza <imosama001@gmail.com>
2022-08-11 11:47:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Dice from './components/Dice';
+import { v4 as uuidv4 } from 'uuid';
+import ReactConfetti from 'react-confetti';
 
 function App() {
+  
+  const [dice,setDice]=useState(allNewDice());
+  const [tenzies,setTenzies]=useState(false);
+
+  useEffect(()=>{
+    const allHeld=dice.every(die=>die.isHeld)
+    const firstValue=dice[0].value;
+    const allSameValue=dice.every(die=>die.value===firstValue);
+    if(allHeld && allSameValue){
+      setTenzies(true)
+      console.log("you won");
+    }
+  },[dice])
+  function generateNewDie(i){
+    return{
+      value:Math.ceil(Math.random()*6),
+      isHeld:false,
+      id:uuidv4()
+    }
+  }
+
+  function allNewDice(){
+    const newDice=[];
+    for(let i=0;i<10;i++){
+      newDice[i]=generateNewDie(i)
+    }
+    return  newDice
+  }
+
+  function rollDice(){
+    if(!tenzies){
+    setDice(oldDice=>oldDice.map(die=>{
+            return die.isHeld ? die : generateNewDie()
+         }))
+       }
+       else{
+        setTenzies(false);
+        setDice(allNewDice())
+       }
+    }
+  function holdDice(id){
+  setDice(oldDice=>oldDice.map(die=>{
+    return die.id===id ? {...die,isHeld:!die.isHeld}:
+    die
+  }))
+  }
+
+  const diceElements=dice.map(die=>
+  <Dice 
+  key={die.id} 
+  value={die.value} 
+  isHeld={die.isHeld} 
+  holdDice={()=>holdDice(die.id)}
+  />)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+      {tenzies && <ReactConfetti/>}
+      <h1 className='title'>Tenzies Game</h1>
+      <p className='instructions'>Roll untill all dice are the same. Click each die to freez
+      it at its current value between the rolls.</p>
+      <div className='dice--container'>
+        {diceElements}
+
+      </div>
+      <button className='roll--dice' onClick={rollDice}>{tenzies? "Restart":"Roll"}</button>
+    </main>
   );
 }
 
---
+++ src/components/Dice.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function Dice(props){
+    const styles={
+        backgroundColor: props.isHeld ?"#59E391":"white"
+        
+    }
+return (
+    <div className='dice--face' 
+    style={styles}
+    onClick={props.holdDice}
+    >
+        <h2 className='dice--num' >{props.value}</h2>
+    </div>
+)
+}
+export default Dice;
